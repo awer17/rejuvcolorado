7253b7f728337faef4a419f375db4d77641199f7
Siarhei Siarheyeu <95449526+awer17@users.noreply.github.com>
2023-04-15 00:12:45 -0600

    feat: servises js

---
+++ script/descripcion_servises.js
@@ -0,0 +1,80 @@
+const servisesList = document.getElementById("services_list", true);
+const pagination = document.querySelector(".pagination");
+const selectServises = document.querySelector('#select_servises');
+const servisesListLeft = document.querySelector("._left-items")
+const descripcion = document.querySelector("._descripcion")
+const slider = document.querySelector("._slider")
+
+
+
+
+async function getSliderServis() {
+            
+    const file = './json/services.json';
+    let response = await fetch(file, {
+        method: 'GET'
+    });
+    if(response.ok) {
+        let result = await response.json();
+        servicesDescripcion(result);
+        
+        ;
+    } else {
+        throw new Error(`!!!!!!!!!!! ${err}`)
+    }
+}
+
+function servicesDescripcion(data){
+    descripcion.innerHTML = '';
+    data.forEach(element => {
+        if(element.nameServes === serviseValue){
+            descripcion.innerHTML = `<button  onclick="btnReturn()" id="return" class="btn_white">
+            return
+        </button>
+        <h3>${element.nameServes}</h3>
+        <div class="_wrapper">
+            <p class="_main">${element.description}</p>
+            <div class="_grafic">
+                <img src="${element.img}" alt="${element.nameServes}">
+                <div class="_shadow"></div>
+            </div>    
+        </div>
+        <div class="_additional">
+            ${element.superDescription}
+        </div>
+            `
+        }
+    });
+}
+
+function classAdd (){
+    descripcion.classList.add("_active");
+    slider.classList.add("_disabled");
+}
+
+function btnReturn () {
+    descripcion.classList.remove("_active");
+    slider.classList.remove("_disabled");
+
+}
+
+
+servisesList.addEventListener('click' , function(event){
+    selectServises.setAttribute("disabled","disabled");
+});
+
+servisesList.addEventListener('change' , function(event){
+    serviseValue = servisesList.value
+    classAdd();
+    getSliderServis()
+    
+});
+
+
+servisesListLeft.addEventListener('click',function(event){
+    serviseValue = event.target.innerHTML;
+    classAdd();
+    getSliderServis()
+
+})
+
---
+++ script/dist/descripcion_servises.dev.js
@@ -0,0 +1,82 @@
+"use strict";
+
+var servisesList = document.getElementById("services_list", true);
+var pagination = document.querySelector(".pagination");
+var selectServises = document.querySelector('#select_servises');
+var servisesListLeft = document.querySelector("._left-items");
+var descripcion = document.querySelector("._descripcion");
+var slider = document.querySelector("._slider");
+
+function getSliderServis() {
+  var file, response, result;
+  return regeneratorRuntime.async(function getSliderServis$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          file = './json/services.json';
+          _context.next = 3;
+          return regeneratorRuntime.awrap(fetch(file, {
+            method: 'GET'
+          }));
+
+        case 3:
+          response = _context.sent;
+
+          if (!response.ok) {
+            _context.next = 12;
+            break;
+          }
+
+          _context.next = 7;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 7:
+          result = _context.sent;
+          servicesDescripcion(result);
+          ;
+          _context.next = 13;
+          break;
+
+        case 12:
+          throw new Error("!!!!!!!!!!! ".concat(err));
+
+        case 13:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
+
+function servicesDescripcion(data) {
+  descripcion.innerHTML = '';
+  data.forEach(function (element) {
+    if (element.nameServes === serviseValue) {
+      descripcion.innerHTML = "<button  onclick=\"btnReturn()\" id=\"return\" class=\"btn_white\">\n            return\n        </button>\n        <h3>".concat(element.nameServes, "</h3>\n        <div class=\"_wrapper\">\n            <p class=\"_main\">").concat(element.description, "</p>\n            <div class=\"_grafic\">\n                <img src=\"").concat(element.img, "\" alt=\"").concat(element.nameServes, "\">\n                <div class=\"_shadow\"></div>\n            </div>    \n        </div>\n        <div class=\"_additional\">\n            ").concat(element.superDescription, "\n        </div>\n            ");
+    }
+  });
+}
+
+function classAdd() {
+  descripcion.classList.add("_active");
+  slider.classList.add("_disabled");
+}
+
+function btnReturn() {
+  descripcion.classList.remove("_active");
+  slider.classList.remove("_disabled");
+}
+
+servisesList.addEventListener('click', function (event) {
+  selectServises.setAttribute("disabled", "disabled");
+});
+servisesList.addEventListener('change', function (event) {
+  serviseValue = servisesList.value;
+  classAdd();
+  getSliderServis();
+});
+servisesListLeft.addEventListener('click', function (event) {
+  serviseValue = event.target.innerHTML;
+  classAdd();
+  getSliderServis();
+});
